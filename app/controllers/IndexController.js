9bcde23a86c8a398f36559a49be0920cd11364c7
Diego <diegossl94@gmail.com>
2020-08-20 14:22:33 -0300

    Some changes

--- app/controllers/IndexController.js
+++ app/controllers/IndexController.js
@@ -10,12 +10,14 @@ module.exports = {
       const file = request.file
       if (file) {
         const { result } = await visualRecognitionService.classify(file.path)
-        const classifyType = result.images[0].classifiers[0].classes[0].class
-        const classifyPercentage = result.images[0].classifiers[0].classes[0].score
-        return response.status(200).send({
-          classifyType: classifyType,
-          classifyPercentage: `${classifyPercentage * 100}%`
-        })
+        const classes = result.images[0].classifiers[0].classes[0]
+        if (classes) {
+          return response.status(200).send({
+            classifyType: classes.class,
+            classifyPercentage: `${classes.score * 100}%`
+          })
+        }
+        return response.send('A imagem não foi reconhecida.')
       }
       return response.status(400).send('Arquivo inválido ou inexistente.')
     } catch (error) {
